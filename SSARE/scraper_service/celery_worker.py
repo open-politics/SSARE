27ed44cf9de2fc790a66c4c137b77ce0ba28d3da
Jim Wagner <jimvwagner@googlemail.com>
2024-01-23 11:41:37 +0100

    Initial commit

---
+++ SSARE/scraper_service/celery_worker.py
@@ -0,0 +1,31 @@
+from celery import Celery
+from typing import List
+# import module import module
+# from module import function
+import json
+
+# Configure Celery
+celery_app = Celery(
+    "worker",
+    backend="redis://localhost:6379/0",
+    broker="redis://localhost:6379/0"
+)
+
+@celery_app.task
+def scrape_data_task(flags: List[str]):
+    config_json = json.load(open("./scrapers/scrapers_config.json"))
+    
+    # Check location of script to run
+    script_location = config_json["scrapers"]["cnn"]["location"]
+    print("Script location:", script_location)
+    
+    # run script
+    import subprocess
+    subprocess.run(["python", script_location])
+    print("Script run")
+
+    # get data
+    import pandas as pd
+    df = pd.read_csv("data/dataframes/cnn_articles.csv")
+    print("Data loaded")
+    
--- SSARE/scraper_service/main.py
+++ SSARE/scraper_service/main.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 from typing import List
 import importlib
 import json
+from fastapi import Body
+from celery_worker import scrape_data_task
 
 app = FastAPI()
 
@@ -12,33 +14,22 @@ class Article(BaseModel):
     headline: str
     paragraphs: List[str]
 
+
+@app.post("/trigger_scraping")
+async def trigger_scraping():
+    task = scrape_data_task.delay()
+    return {"message": "Scraping started", "task_id": task.id}
+
+
 def get_scraper_config():
-    with open("scraper/config.json") as f:
+    with open("./scrapers/config.json") as f:
         return json.load(f)
 
 @app.post("/scrape")
 def scrape_data(flags: List[str] = Body(...)):
-    from fastapi import Bodyc
-    config = get_scraper_config()
 
     for flag in flags:
-        try:
-            scraper_config = config[flag]
-            scraper_module = importlib.import_module(scraper_config['scraper_location'])
-            scraped_data = scraper_module.run_scraper()
-
-            # Assuming scraped_data is a list of Article objects
-            response = requests.post(
-                "http://postgres_service:8000/receive_raw_articles", 
-                json={"data": [article.dict() for article in scraped_data]}
-            )
-            # Handle response
-        except KeyError:
-            raise HTTPException(status_code=404, detail=f"Scraper for '{flag}' not found")
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
-
-    return {"message": "Scraping completed"}
+        
 
 
 @app.get("/health")
