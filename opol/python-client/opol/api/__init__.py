f2fb8b23a4e89dd5c7641da8ddb3851bf7c4cfe4
Jim Wagner <jimvwagner@googlemail.com>
2024-12-26 15:09:38 +0000

    Updating package

--- opol/python-client/opol/api/__init__.py
+++ opol/python-client/opol/api/__init__.py
@@ -2,6 +2,7 @@ from .articles import Articles
 from .entities import Entities
 from .geo import Geo
 from .classification import Classification
+from .models import *
 from .client_base import BaseClient
 from .scraping import Scraping
 
