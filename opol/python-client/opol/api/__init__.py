004ada2ddb73adc5c3adac90ee58b4712c2925f6
Jim Wagner <jimvwagner@googlemail.com>
2024-12-26 15:12:06 +0000

    Updating package

--- opol/python-client/opol/api/__init__.py
+++ opol/python-client/opol/api/__init__.py
@@ -2,8 +2,8 @@ from .articles import Articles
 from .entities import Entities
 from .geo import Geo
 from .classification import Classification
-from .models import *
+from .models import * as models
 from .client_base import BaseClient
 from .scraping import Scraping
 
-__all__ = ["Articles", "Entities", "Geo", "Classification", "BaseClient", "Scraping"]
+__all__ = ["Articles", "Entities", "Geo", "Classification", "BaseClient", "Scraping", "models"]
